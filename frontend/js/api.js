3f5c04347573a62cba98525c495728a08cade1a2
AMOFAH292 <124503206+AMOFAH292@users.noreply.github.com>
2026-08-26 13:40:43 +0000

    pos works with bugs

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,6 +1,6 @@
 // API Configuration and Utility Functions
 
-const API_BASE_URL = `${window.location.protocol}//${window.location.hostname}:5000/api`;
+const API_BASE_URL = `https://m96fj3tl-5000.uks1.devtunnels.ms/api`;
 
 class APIClient {
   constructor() {
